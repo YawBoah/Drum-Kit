a0d741bd5281a3638ad9fc6c5f7851bec7bbb5ab
YBoah <yawboahene.yb@gmail.com>
2023-10-30 18:37:36 +0000

    Adding a switch loop for the audios

--- index.js
+++ index.js
@@ -12,10 +12,50 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var e = 0; e < numberOfDrumButtons; e++){
     document.querySelectorAll(".drum")[e].addEventListener("click", function(){
        
-        this.style.color ="white";
+        // this.style.color ="white";
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+        
+            default:cocnsole.log(buttonInnerHTML);
+                break;
+        }
     })
 }
 
 
-var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
