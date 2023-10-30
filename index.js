6159b62ce7f42beca5582ab352082fb49a69d1bd
YBoah <yawboahene.yb@gmail.com>
2023-10-30 09:59:09 +0000

    Adding Event Listeners to the buttons

---
+++ index.js
@@ -0,0 +1,16 @@
+//Add Event Listeners to the buttons
+
+// One way
+// document.querySelector(".set").addEventListener("click", function(){
+//     alert("I just got clicked");
+// })
+
+
+// Other way
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var e = 0; e < numberOfDrumButtons; e++){
+    document.querySelectorAll(".drum")[e].addEventListener("click", function(){
+        alert("I just got clicked");
+    })
+}
